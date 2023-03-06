01fa0fcf81084c02353599b63b5c38be5592f92e
Michael Leet <michael@fioprotocol.io>
2023-03-05 20:48:15 -0700

    bd-4272: minor cleanup

--- init.go
+++ init.go
@@ -130,10 +130,14 @@ func init() {
 	// Validate state file exists (either default or file explicitly set on command line)
 	emptyFatal(cnf.stateFile, "state cache file cannot be empty")
 
-	logInfo(fmt.Sprintf("NODEOS API URL: %s", cnf.apiUrl))
-	logInfo(fmt.Sprintf("DB URL:  %s", cnf.dbUrl))
-	logInfo(fmt.Sprintf("WIF:     %s", cnf.wif))
-	logInfo(fmt.Sprintf("PERM:    %s", cnf.permission))
+	logInfo(fmt.Sprintf("NODEOS API URL:  %s", cnf.apiUrl))
+	logInfo(fmt.Sprintf("DB URL:          %s", cnf.dbUrl))
+	logInfo(fmt.Sprintf("WIF:             %s", cnf.wif))
+	logInfo(fmt.Sprintf("PERM:            %s", cnf.permission))
+	logInfo(fmt.Sprintf("Data File:       %s", cnf.stateFile))
+	logInfo(fmt.Sprintf("Min Bundle Tx:   %d", cnf.minBundleTx))
+	logInfo(fmt.Sprintf("Tx Persistence:  %t", cnf.persistTx))
+	logInfo(fmt.Sprintf("Verbose Logging: %t", cnf.verbose))
 
 	var e error
 
